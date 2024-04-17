d08d2dbdca5421d3d6a083c1cc192bafd0657fd5
Gabriel Freitas Willig <gwillig@pop-os.localdomain>
2024-04-17 19:16:57 -0300

    sending large txt files

--- client.py
+++ client.py
@@ -7,10 +7,12 @@ client.bind(("localhost", random.randint(8000, 9000)))
 
 name = input("Nickname: ")
 
+BUFFER = 32
+
 def receive():
     while True:
         try:
-            message, _ = client.recvfrom(2048)
+            message, _ = client.recvfrom(BUFFER)
             print(message.decode())
         except:
             pass
@@ -24,5 +26,10 @@ while True:
     message = input("")
     if message == "!q":
         exit()
+    # elif message.startswith("GET /file"):
+    #     split = message.split(' ')
+    #     file = split[2]
+    #     client.sendto(f"{name} requested GET: {file}".encode(), ("localhost", 9999))
+    # GET /file --filename=filename.txt
     else:
         client.sendto(f"{name}: {message}".encode(), ("localhost", 9999))
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import queue
-
+import io
 #IP: 192.168.56.1
 
 messages = queue.Queue()
@@ -10,11 +10,12 @@ clients = []
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("localhost", 9999))
 
+BUFFER = 32
+
 def receive():
     while True:
         try:
-            
-            message, addr = server.recvfrom(1024)
+            message, addr = server.recvfrom(2048)
             messages.put((message, addr))
         except:
             pass
@@ -22,7 +23,29 @@ def broadcast():
     while True:
         while not messages.empty():
             message, addr = messages.get()
-            print(message.decode())
+            split = message.decode().split(' ')
+            file = split[1:3]
+            if(file == ['GET', '/file']):
+                try:
+                    fileName = message.decode().split("=")
+                    fileName = fileName[1]
+                    with open(f"{fileName}", "rb") as f:
+                        contents = f.read()
+                        for i in range(0,len(contents), BUFFER):
+                            if(i + BUFFER > len(contents)):
+                                substring = contents[i:]
+                            else:
+                                substring = contents[i:i+BUFFER]
+                            print(substring, end="")
+                            server.sendto(substring, client)
+                            # with open(f"{fileName}", "wb+") as file:
+                            #     while...:
+                            #         file.write(contents)
+                except Exception as e:
+                    server.sendto(f"File not {fileName} found!".encode(), client)
+                    continue
+            else:    
+                print(message.decode())
             if addr not in clients:
                 clients.append(addr)
             for client in clients:
