6c087d28df86659f83e05d99b57489bfe61417a9
gabrielwillig <gabrielwillig@gmail.com>
2024-06-24 01:32:38 -0300

    01100100 01100101 01110110 01101111 00100000 01110101 01101101 00100000 01100010 01100001 01100010 01100001 01101111 00100000 01110000 01110010 01101111 00100000 01110000 01100101 01100100 01110010 01101111

--- tcp/client.py
+++ tcp/client.py
@@ -1,5 +1,6 @@
 import socket
 import struct 
+import threading
 from utils import PORT, BUFFER_SIZE, CHECKSUM_SIZE, PACKET_SIZE, FORMAT, DISCONNECT_MSG, SERVER_IP, calculate_sha_checksum
 
 IP = SERVER_IP#socket.gethostbyname(socket.gethostname())
@@ -13,104 +14,105 @@ class TCP_Client:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((IP, PORT))
         self.last_packet = -1
+        self.connected = True
+        self.old_checksum = b""
+        self.isTransmitting = False
+        self.file = None
+        self.file_name = None
+        self.packets = 0
         print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
 
     def execute(self):
-        connected = True
-        while connected:
-            
-            message = input("> ")
+        input_thread = threading.Thread(target=self.read_input)  # Thread para ler entradas do teclado
+        input_thread.start()
 
-            self.client.send(message.encode(FORMAT))
-
-            if(message == DISCONNECT_MSG):
-                connected = False
-            elif("GET" in message):
-                split = message.split(' ')    
-                if(len(split) > 1):
-                    file_name = split[1]
-                    self.get_request(file_name)
-            elif("ACK" in message):
-                print("You cannot send an ACK!")
-                continue 
+        while self.connected:
+            server_msg = self.receive()
+            if(self.isTransmitting):
+                self.handle_file(server_msg)
             else:
-                server_msg = self.receive(True)  
-                print(f"[SERVER] {server_msg}")
+                server_msg = server_msg.decode(FORMAT)
+                print(server_msg)
 
         self.client.close()
 
-    def get_request(self, file_name):
-
-        message = self.receive(True)
-
-        print(f"[SERVER] {message}")
-
-        old_checksum = self.receive()
-
-        message = self.receive(True)
-
-        split = message.split(' ')
-
-        if(split[0] == "FOUND"):
+    def handle_file(self, msg: str):
+        if(msg.startswith(b"[CHECKSUM]")):
+            msg = msg.replace(b"[CHECKSUM]", b"")
+            self.old_checksum = msg
+            return
+        elif(msg.startswith(b"[FOUND]")):
+            msg = msg.decode(FORMAT)
+            split = msg.split(" ")
             self.last_packet = 0
-            print(message)
-            packets = int(split[1])
-            file = open(f"../duplicate/{file_name}", "wb+")
-            while self.last_packet < packets:
+            print(msg)
+            self.packets = int(split[1])
+            self.ack_request(self.file_name, self.last_packet+1)
+            return
+        elif(msg.startswith(b"[ERROR]")):
+            msg = msg.decode(FORMAT)
+            print(msg)
+            self.isTransmitting = False
+            return
+        else:
+            content = msg[PACKET_SIZE:]
+            packet_part = struct.unpack(">q", msg[0:PACKET_SIZE])[0]
+            if packet_part != (self.last_packet + 1):
+                return
+            print(f"Receiving package: {packet_part}")
+            self.file.write(content)
+            self.last_packet += 1
+            if(self.last_packet < self.packets):
                 try:
-                    self.ack_request(file_name, self.last_packet+1, file)
-                except ChecksumFailed:
-                    print(f'Checksum failed in packet: {self.last_packet+1}')
+                    self.ack_request(self.file_name, self.last_packet+1)
                 except TimeoutError:
                     print(f'Timeout error in packet: {self.last_packet+1}')
                 except Exception as e:
                     print(f'Error in packet {self.last_packet+1} -> {e}')
-            print(f"Imported file: {file_name}")
-            file.close()
-            with open(f"../duplicate/{file_name}", 'rb') as f:
-                new_checksum = calculate_sha_checksum(f.read())
-            if(new_checksum != old_checksum):
-                print(f"Invalid SHA checksum!")
-                self.client.send((f"GET {file_name}").encode(FORMAT))
-                self.get_request(file_name)
             else:
-                print("Valid SHA Checksum!")
-        elif(split[0] == "ERROR"):
-            print(message)
-            return
-        else:
-            print(f'Unknown message: {message}')
-            return
-
-    def ack_request(self, file_name, packet: int, file):
-        msg = (f'ACK {file_name} {packet}').encode(FORMAT)
-
-        self.client.send(msg)
-
-        message = self.receive(True)
-
-        #print(f"[SERVER] {message}")
-
-        message = self.receive()
-
-        content = message[PACKET_SIZE:]
-        packet_part = struct.unpack(">q", message[0:PACKET_SIZE])[0]
-
-        if packet_part != (self.last_packet + 1):
-            print(f"Wrong package: {packet_part}")
+                print(f"Imported file: {self.file_name}")
+                self.file.close()
+                with open(f"../duplicate/{self.file_name}", 'rb') as f:
+                    new_checksum = calculate_sha_checksum(f.read())
+                if(new_checksum != self.old_checksum):
+                    print("Invalid SHA Checksum!")
+                    self.client.send((f"GET {self.file_name}").encode(FORMAT))
+                    self.get_request(self.file_name)
+                else:
+                    print("Valid SHA Checksum!")
+                self.isTransmitting = False
             return
         
-        print(f"Receiving package: {packet_part}")
+    def read_input(self):
+        while self.connected:
+            message = input()
+            if(message == DISCONNECT_MSG):
+                self.connected = False
+            elif("GET" in message):
+                split = message.split(' ')    
+                file_name = split[1]
+                self.get_request(file_name)
+            elif("ACK" in message):
+                print("[ERROR] You cannot send an ACK!")
+            self.client.send(message.encode(FORMAT))
 
-        file.write(content)
-        self.last_packet += 1
+    def get_request(self, file_name):
+        self.file = open(f"../duplicate/{file_name}", "wb+")
+        self.file_name = file_name
+        self.isTransmitting = True
 
+    def ack_request(self, file_name, packet: int):
+        msg = (f'ACK {file_name} {packet}').encode(FORMAT)
+        self.client.send(msg)
 
-    def receive(self, isMessage: bool = False):
+    def receive(self):
         msg = self.client.recv(BUFFER_SIZE)
-        if(isMessage == True):
-            msg = msg.decode(FORMAT)
         return msg
+        # if(msg.startswith(b"[CHECKSUM]")):
+        #     msg = msg.replace(b"[CHECKSUM]", b"")
+        #     self.old_checksum = msg
+        #     return msg
+        # return msg.decode(FORMAT)
 
 if __name__ == "__main__":
     client = TCP_Client()
--- tcp/server.py
+++ tcp/server.py
@@ -14,18 +14,28 @@ class TCP_Server:
         self.server.bind((IP, PORT))
         self.server.listen()
         print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+        self.clients = []  # Lista para armazenar as conexões dos clientes
 
     def execute(self):
+        input_thread = threading.Thread(target=self.read_input)  # Thread para ler entradas do teclado
+        input_thread.start()
+
         while True:
             try:
                 conn, addr = self.server.accept()
+                self.clients.append(conn)  # Adiciona a nova conexão à lista de clientes
                 thread = threading.Thread(target=self.handle_client, args=(conn,addr))
-                thread.start()       
-                print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+                thread.start()    
+                print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 2}")  # Exclui a thread de input
             except socket.timeout:
                 continue
             except KeyboardInterrupt:
                 break
+
+    def read_input(self):
+        while True:
+            message = input()  # Lê entrada do teclado
+            self.broadcast_to_all(f"[SERVER-MESSAGE] {message}")
 
     def handle_client(self, conn, addr):
         print(f"[NEW CONNECTION] {addr} connected.")    
@@ -36,37 +46,32 @@ class TCP_Server:
 
             print(f"[{addr}] {msg}")
 
-            message = f"Message received: {msg}"
-            self.broadcast(message, conn)
-
-            if msg == DISCONNECT_MSG:
-                connected = False
-                break
-
             split = msg.split(' ')
 
-            if(len(split) > 1):
+            if(msg == DISCONNECT_MSG):
+                connected = False
+            elif(len(split) > 1):
                 file_name = split[1]
                 if(split[0] == "GET"):
                     self.get_request(conn, file_name)
                 elif(split[0] == "ACK"):
                     packet = int(split[2])
                     self.ack_request(conn, file_name, packet)
-
+            self.broadcast(f"[SERVER] Received message: {msg}", conn)    
         conn.close()
 
     def get_request(self, conn, file_name):
-        checksum = bytes(" ", 'utf-8')
+        checksum = b"[CHECKSUM]"
         try:
             with open(f"../assets/{file_name}", 'rb') as f:
                 content = f.read()
-                checksum = calculate_sha_checksum(content)
+                checksum = checksum + calculate_sha_checksum(content)
                 packets = math.ceil(len(content)/DATA_SIZE)
-                message = f'FOUND {packets} packets!'
+                message = f'[FOUND] {packets} packets!'
         except FileNotFoundError:
-            message = f'ERROR File: {file_name} not found!'    
+            message = f'[ERROR] File: {file_name} not found!'    
         except Exception as e:
-            message = f'ERROR {e}!'
+            message = f'[ERROR] {e}!'
         finally:
             self.broadcast(checksum, conn)
             self.broadcast(message, conn)
@@ -77,23 +82,33 @@ class TCP_Server:
                 content = f.read()
                 packets = math.ceil(len(content)/DATA_SIZE)
                 if packet > packets:
-                    message = 'ERROR exceeded maximum packet number'
+                    message = '[ERROR] exceeded maximum packet number'
                 else:
                     start = DATA_SIZE * (packet - 1)
                     end = start + DATA_SIZE
                     response = content[start:end]
                     message = struct.pack(">q", packet) + response
         except FileNotFoundError:
-            message = f'ERROR File {file_name} not found!'    
+            message = f'[ERROR] File {file_name} not found!'    
         except Exception as e:
-            message = f'ERROR {e}!'
+            message = f'[ERROR] {e}!'
         finally:
             self.broadcast(message, conn)
 
     def broadcast(self, msg, conn):
         if type(msg) is str:
-           msg = bytes(msg, 'utf-8')
+           msg = bytes(msg, FORMAT)
         conn.send(msg)
+
+    def broadcast_to_all(self, msg):
+        if type(msg) is str:
+            msg = bytes(msg, FORMAT)
+        for client in self.clients:
+            try:
+                client.send(msg)
+            except Exception as e:
+                print(f"[ERROR] sending message to client: {e}")
+                self.clients.remove(client)
 
 if __name__ == "__main__":
     server = TCP_Server()
